f14dc7164ecda9217fbbf8dcd68d3b74bfe8588b
EllysonRPT <reptile1284@gmail.com>
2023-09-18 20:17:58 -0300

    18/09

--- script.js
+++ script.js
@@ -1,155 +1,42 @@
-// // Declaração das variáveis que armazenam os caminhos das imagens
-// var imgApa = "img/apagada.jpg"; // Imagem vermelha
-// var imgAce = "img/ybxlO.jpg";    // Imagem amarela
-// var imgQue = "img/quebrada.jpg";      // Imagem verde
-
-// // Definição da função "trocar" que alterna as imagens
-// function trocar() {
-//     // Define o atributo "src" da imagem com id "figura" para o caminho da imagem vermelha
-//     document.getElementById("figura").src = imgApa;
-
-//     // Troca os valores das variáveis para a próxima imagem a ser exibida
-//     let aux = imgApa;
-//     imgApa= imgAce;
-//     imgAce = aux;
-
-//     //==============
-
-//     // Troca os valores das variáveis novamente para a próxima imagem a ser exibida
-//     let aux1 =imgAce ;
-//     imgAce = imgQue;
-//     imgQue = aux1;
-
-
-
-// }
-// var imagem = document.getElementById("minhaImagem");
-
-// // Adiciona um evento de clique à imagem
-// imagem.addEventListener("click", function() {
-//     document.getElementById("figura").src = imgApa;
-
-//     // Troca os valores das variáveis para a próxima imagem a ser exibida
-//     let aux = imgApa;
-//     imgApa= imgAce;
-//     imgAce = aux;
-
-//     //==============
-
-//     // Troca os valores das variáveis novamente para a próxima imagem a ser exibida
-//     let aux1 =imgAce ;
-//     imgAce = imgQue;
-//     imgQue = aux1;
-//     // Coloque aqui o código que deseja executar quando a imagem for clicada
-//     // Por exemplo, trocar a imagem ou realizar alguma ação
-//     alert("A imagem foi clicada!");
-// });
-
-// function resetarImagens() {
-//     document.getElementById("imagem1").src = "img/ybxlO.jpg"; // Reset para a imagem 1
-//     document.getElementById("imagem1").alt = "Imagem 1";
-//     contadorTrocas = 0; // Reseta o contador de trocas
-// }
-
-
-// var imgApa = "img/apagada.jpg"; // Imagem vermelha
-// var imgAce = "img/ybxlO.jpg";    // Imagem amarela
-// var imgQue = "img/quebrada.jpg";      // Imagem verde
-
-// // Variáveis para controlar a troca de imagens
-// var contadorTrocas = 0;
-// var imagens = [imgApa, imgAce];
-// var imagemAtual = 0;
-
-// // Função para trocar a imagem
-// function trocarImagem() {
-//     contadorTrocas++;
-
-//     if (contadorTrocas % 5 === 0) {
-//         document.getElementById("minhaImagem").src = imgQue; // Troca para a imagem 3
-//         document.getElementById("minhaImagem").alt = "Imagem 3";
-//     } else {
-//         imagemAtual = (imagemAtual + 1) % 2;
-//         document.getElementById("minhaImagem").src = imagens[imagemAtual]; // Troca para a próxima imagem
-//         document.getElementById("minhaImagem").alt = "Imagem " + (imagemAtual + 1);
-//     }
-// }
-
-// // Função para resetar a imagem e o contador de trocas
-// function resetarImagens() {
-//     document.getElementById("minhaImagem").src = imgAce; // Reset para a imagem 1
-//     document.getElementById("minhaImagem").alt = "Imagem 1";
-//     contadorTrocas = 0; // Reseta o contador de trocas
-//     imagemAtual = 0; // Reseta a imagem atual
-// }
-
+// Obtém uma referência ao elemento HTML com o ID "minhaImagem" (presumivelmente uma imagem de lâmpada)
 let lampada = document.getElementById("minhaImagem");
+
+// Inicializa variáveis para rastrear o estado da lâmpada e o número de vezes que ela foi acesa
 let cont = 0;
 let lampadaOk = true;
 let lampadaAcesa = false;
+
+// Função que lida com o evento de clique na lâmpada
 function acender() {
-    if (lampadaAcesa==false) {
+    if (lampadaAcesa == false) {
+        // Se a lâmpada não estiver acesa, a imagem da lâmpada é trocada para uma imagem acesa
         lampadaAcesa = true;
-        lampada.src = "img/ybxlO.jpg";
+        lampada.src = "img/ybxlO.png";
         cont++;
+
+        // Se a lâmpada for acesa mais de 5 vezes, ela é quebrada
         if (cont > 5) {
-            lampada.src = "img/quebrada.jpg";
+            lampada.src = "img/quebrada.png";
             lampadaOk = false;
         }
-    }else if(lampadaOk){
+    } else if (lampadaOk) {
+        // Se a lâmpada estiver acesa, a imagem  é trocada para apagada
         lampadaAcesa = false;
-        lampada.src = "img/apagada.jpg";
+        lampada.src = "img/apagada.png";
     }
 }
 
+// Função que reseta o estado da lâmpada
 function trocar() {
     cont = 0;
-    lampada.src = "img/apagada.jpg";
+    lampada.src = "img/apagada.png";
     lampadaOk = true;
     lampadaAcesa = false;
-
-}
-lampada.addEventListener("click",acender);
-
-
-
-function salvarEscolha(opcao) {
-    localStorage.setItem('opcaoSelecionada', opcao);
 }
 
-// Mapeia as opções do usuário para os caminhos das imagens
-const imagensSelecao = {
-    'Pedra': 'img/PedraSemFundo.png',
-    'Papel': 'img/PapelSemFundo.png',
-    'Tesoura': 'img/TesouraSemFundo.png'
-};
+// um clique chama a função "acender", quando a lâmpada é clicada
+lampada.addEventListener("click", acender);
 
-let escolhaUsuario = '';
-const ConfSelec = document.getElementById('ConfSelec');
-let opcaoSelecionada = ''; // Variável para armazenar a opção selecionada
 
-function verificarSelecao() {
-    // se seleção estiver vazia não permite confirmar seleção
-    if (escolhaUsuario !== "") {
-        ConfSelec.disabled = false;
-    } else {
-        ConfSelec.disabled = true;
-    }
-}
-
-function Selecao(opcao) {
-    escolhaUsuario = opcao;
-    // alert('Escolha do usuário: ' + escolhaUsuario);
-    selecaoTexto.textContent = `Você selecionou: ${opcao}`;
-    verificarSelecao(); // Verifica a seleção sempre que uma opção é escolhida
-    opcaoSelecionada = opcao;
-}
-
-function confirmarEscolha() {
-    //Armazena localmente o valor de opcaoSelcionada
-    localStorage.setItem('opcaoSelecionada', opcaoSelecionada);
-    var ResultadoURL = "Resultado.html";
-    window.location.href = ResultadoURL;
-}
 
 
--- scriptHard.js
+++ scriptHard.js
@@ -6,10 +6,9 @@ const imagensSelecao = {
     'Tesoura': 'img/TesouraSemFundo.png'
 };
 
-let escolhaUsuario = '';
-let caminhoEscolhaUsuario = '';
-const ConfSelec = document.getElementById('ConfSelec');
-let opcaoSelecionada = ''; // Variável para armazenar a opção selecionada
+let escolhaUsuario = '';          // Variável para armazenar a opção selecionada
+let caminhoEscolhaUsuario = '';   //Variável para converter a escolha para o caminho da imagem correspondente 
+const ConfSelec = document.getElementById('ConfSelec'); //Variavel para controle do botão de confirmar seleção
 
 function verificarSelecao() {
     // se seleção estiver vazia não permite confirmar seleção
@@ -22,14 +21,12 @@ function verificarSelecao() {
 
 //=============Seleção do Usuario=============================================
 function Selecao(opcao) {
-    escolhaUsuario = opcao;
-    const imagemUsuario = document.getElementById('imagem1');
-    // alert('Escolha do usuário: ' + escolhaUsuario);
-    selecaoTexto.textContent = `Você selecionou: ${opcao}`;
-    verificarSelecao(); // Verifica a seleção sempre que uma opção é escolhida
-    opcaoSelecionada = opcao;
-    // Atualiza a imagem do usuário com base na escolha
-    imagemUsuario.src = imagensSelecao[opcao]
+    escolhaUsuario = opcao; // define a variavel com o valor escolhido pelo usuario "pedra, papel ou tesoura"
+    let imagemUsuario = document.getElementById('imagem1');  // variavel define caminho imagem do usuario
+    selecaoTexto.textContent = `Você selecionou: ${opcao}`;  //Muda o texto do selecaoTexto
+    verificarSelecao(); // Verifica a seleção sempre que uma opção é escolhidas
+    // Atualiza a imagem do usuário (imagem1) com base na escolha
+    imagemUsuario.src = imagensSelecao[opcao] // a variavel opção é a chave para acessar o caminho da imagem escolhida
 
     // "Converte" a escolha do usuario para o caminho da imagem correspondente >>> será usado na function escolheVencedor
     if (escolhaUsuario === 'Pedra') {
@@ -46,14 +43,14 @@ function Selecao(opcao) {
 
 //============Sorteia a imagem do computador==================================
 function sortearImagem() {
-    const imagens = ["img/PedraSemFundo.png", "img/PapelSemFundo.png", "img/TesouraSemFundo.png"];
+    const imagens = ["img/PedraSemFundo.png", "img/PapelSemFundo.png", "img/TesouraSemFundo.png"]; // nova variavel com o caminho das imagens 
     // Gera um número aleatório entre 0 e 2 (a multiplicação pelo comprimento do vetor imagens quer dizer que ira sortear um número entre 0 e 2)
-    //Math.random() sorteia um numero ente 0 e 1
+    //Math.random() sorteia um numero ente 0 e 1 (ex:0,1;0,001;etc)
     //Math.floor() arrendonda um número para inteiro
-    const numeroAleatorio = Math.floor(Math.random() * imagens.length);
+    let numeroAleatorio = Math.floor(Math.random() * imagens.length);
 
     // Pega a imagem no segundo card
-    const minhaImagemSegundoCard = document.getElementById('imagem2');
+    let minhaImagemSegundoCard = document.getElementById('imagem2');
 
     // Define o caminho da imagem com base no número aleatório gerado
     minhaImagemSegundoCard.src = imagens[numeroAleatorio];
@@ -61,7 +58,7 @@ function sortearImagem() {
     return imagens[numeroAleatorio];
 
 }
-//============Calacula o vencedor==================================
+//============Calcula o vencedor==================================
 
 function escolheVencedor(caminhoEscolhaUsuario, escolhaComputador) {
     if (caminhoEscolhaUsuario === escolhaComputador) {
@@ -80,17 +77,22 @@ function escolheVencedor(caminhoEscolhaUsuario, escolhaComputador) {
 //============Mostra a Section com o resultado==================================
 
 function mostrarResultado() {
+
+    //variaveis para controlar quando a section correta deve aparecer
     var mostraResultado = document.getElementById("Resultado");
     var mostraGame = document.getElementById("Game");
     var botaoSelecao = document.getElementById("botaoSelecao");
 
     // Chame a função para sortear a imagem aleatória e a armazena o resultado na variavel escolhaComputador
-    const escolhaComputador = sortearImagem();
-    const vencedor = escolheVencedor(caminhoEscolhaUsuario, escolhaComputador);
+    let escolhaComputador = sortearImagem();
+    // chama a função para decidir o vencedor
+    let vencedor = escolheVencedor(caminhoEscolhaUsuario, escolhaComputador); // variaveis com os caminhos das imagens do usuario e do computador
 
-    selecaoTexto.textContent = `Resultado: ${vencedor}`;
+    // texto para  mostrar o resultado do jogo
+    selecaoTexto.textContent = `Resultado: ${vencedor}`; // variavel vencedor tera o valor "return" conforme a função escolheVencedor
 
-    if (mostraResultado.style.display === "none") {
+    // Condições if para ser visivel na tela as section certas 
+    if (mostraResultado.style.display === "none") { 
         mostraGame.style.display = "none";
         botaoSelecao.style.display = "none";
         mostraResultado.style.display = "block";
@@ -98,4 +100,3 @@ function mostrarResultado() {
         mostraResultado.style.display = "none";
     }
 }
-
